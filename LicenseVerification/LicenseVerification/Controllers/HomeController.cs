50816509d8cbd81786080919b3f685f527bf7b82
Xinxin-InfoTrack <xinxin.li@infotrack.com.au>
2017-11-03 11:58:59 +1100

    add the new ui

--- LicenseVerification/LicenseVerification/Controllers/HomeController.cs
+++ LicenseVerification/LicenseVerification/Controllers/HomeController.cs
@@ -17,12 +17,11 @@ namespace LicenseVerification.Controllers
             return View();
         }
 
-        public JsonResult Analyse(IndividualData data)
+
+
+        public JsonResult AnalyseImage1(IndividualData data)
         {
-            var valid = true;
-
-
-
+            var valid = true; 
 
             //todo: need tina to add the google api account information to verify the name
 
@@ -51,5 +50,97 @@ namespace LicenseVerification.Controllers
         }
 
 
+        public JsonResult BetterAnalyseImage1(IndividualData data)
+        {
+            var valid = true;
+
+            //todo: need tina to add the google api account information to verify the name
+
+            //1: call google api to get the text descirption
+            var description = new GoogleVisiualAPI().Execute(data.Image);
+
+            //2: verfify if name match and return the information 
+            var firstNameWithNoSpace = data.FirstName.Replace(" ", "");
+            if (!description.Contains(firstNameWithNoSpace) || !description.Contains(data.LastName))
+            {
+                valid = false;
+            }
+
+            var licenseNoWithNoSpace = data.License.Replace(" ", "");
+            if (!description.Contains(licenseNoWithNoSpace))
+            {
+                valid = false;
+            }
+
+            if (valid)
+            {
+                return Json(new MatchModel { NameMatch = true, BirthdayMatch = false, LicenseMatch = true });
+            }
+
+            return Json(new MatchModel { NameMatch = true, BirthdayMatch = false, LicenseMatch = false });
+        }
+
+
+        public JsonResult AnalyseImage2(IndividualData data)
+        {
+            var valid = true;
+            
+
+            //todo: need tina to add the google api account information to verify the name
+
+            //1: call google api to get the text descirption
+            var description = new GoogleVisiualAPI().Execute(data.Image);
+
+            //2: verfify if name match and return the information 
+            var firstNameWithNoSpace = data.FirstName.Replace(" ", "");
+            if (!description.Contains(firstNameWithNoSpace) || !description.Contains(data.LastName))
+            {
+                valid = false;
+            }
+
+            var licenseNoWithNoSpace = data.License.Replace(" ", "");
+            if (!description.Contains(licenseNoWithNoSpace))
+            {
+                valid = false;
+            }
+
+            if (valid)
+            {
+                return Json(new MatchModel { NameMatch = true, BirthdayMatch = false, LicenseMatch = true });
+            }
+
+            return Json(new MatchModel { NameMatch = true, BirthdayMatch = false, LicenseMatch = false });
+        }
+
+        public JsonResult BetterAnalyseImage2(IndividualData data)
+        {
+            var valid = true;
+
+
+            //todo: need tina to add the google api account information to verify the name
+
+            //1: call google api to get the text descirption
+            var description = new GoogleVisiualAPI().Execute(data.Image);
+
+            //2: verfify if name match and return the information 
+            var firstNameWithNoSpace = data.FirstName.Replace(" ", "");
+            if (!description.Contains(firstNameWithNoSpace) || !description.Contains(data.LastName))
+            {
+                valid = false;
+            }
+
+            var licenseNoWithNoSpace = data.License.Replace(" ", "");
+            if (!description.Contains(licenseNoWithNoSpace))
+            {
+                valid = false;
+            }
+
+            if (valid)
+            {
+                return Json(new MatchModel { NameMatch = true, BirthdayMatch = false, LicenseMatch = true });
+            }
+
+            return Json(new MatchModel { NameMatch = true, BirthdayMatch = false, LicenseMatch = false });
+        }
     }
 }
--- LicenseVerification/LicenseVerification/Scripts/Index/Index.js
+++ LicenseVerification/LicenseVerification/Scripts/Index/Index.js
@@ -17,40 +17,175 @@
 
 $(function () {
     
-    $('form').submit(function (event) {
-        $('.loader').show();
-        event.preventDefault();
 
-        if ($(this).valid()) {
-            var formdata = new FormData($(this).get(0));
+    //var firstName1 = $("#firstName1").value();
+    //var firstName2 = $("#firstName2").value();
+    //var firstDob = $("#firstDob").value();
+    //var firstLicense = $("#firstLicense").value();
+
+
+
+ 
+
+     
+    $('#firstAnalyse').click(function () {
+        $('.loader').show();
+
+        var firstImageData = {
+            FirstName: $("#firstName1").val(),
+            LastName: $("#firstName2").val(),
+            Birthday: $("#firstDob").val(),
+            License: $("#firstLicense").val()
+        }
+
+        $.ajax({
+            url: "/Home/AnalyseImage1",
+            type: "post",
+            data: JSON.stringify(firstImageData),
+            processData: false,
+            contentType: "application/json",
+            success: function (result) {
+                $('#nameMatch').removeClass();
+                $('#birthMatch').removeClass();
+                $('#matchResult').removeClass();
+                $('#nameMatch').addClass(result.NameMatch ? 'matchSuccess' : 'matchFail');
+                $('#birthMatch').addClass(result.BirthdayMatch ? 'matchSuccess' : 'matchFail');
+                $('#licenseMatch').addClass(result.LicenseMatch ? 'matchSuccess' : 'matchFail');
+                if (result.NameMatch && result.BirthdayMatch && result.LicenseMatch) {
+                    $('#matchResult').addClass('SuccessResult');
+                } else {
+                    $('#matchResult').addClass('FailResult');
+                }
+                setTimeout(function () {
+                    $('.loader').hide();
+                    $('#resultPage').show();
+                }, 1000);
+            },
+            complete: function () {
+            }
+        });
+         
+    });
+
+    $('#firstBetterAnalyse').click(function () {
+        $('.loader').show();
+
+        var firstImageData = {
+            FirstName: $("#firstName1").val(),
+            LastName: $("#firstName2").val(),
+            Birthday: $("#firstDob").val(),
+            License: $("#firstLicense").val()
+        }
+
+        $.ajax({
+            url: "/Home/BetterAnalyseImage1",
+            type: "post",
+            data: JSON.stringify(firstImageData),
+            processData: false,
+            contentType: "application/json",
+            success: function (result) {
+                $('#nameMatch').removeClass();
+                $('#birthMatch').removeClass();
+                $('#matchResult').removeClass();
+                $('#nameMatch').addClass(result.NameMatch ? 'matchSuccess' : 'matchFail');
+                $('#birthMatch').addClass(result.BirthdayMatch ? 'matchSuccess' : 'matchFail');
+                $('#licenseMatch').addClass(result.LicenseMatch ? 'matchSuccess' : 'matchFail');
+                if (result.NameMatch && result.BirthdayMatch && result.LicenseMatch) {
+                    $('#matchResult').addClass('SuccessResult');
+                } else {
+                    $('#matchResult').addClass('FailResult');
+                }
+                setTimeout(function () {
+                    $('.loader').hide();
+                    $('#resultPage').show();
+                }, 1000);
+            },
+            complete: function () {
+            }
+        });
+
+    });
+
+     
+
+
+
+    $('#secondAnalyse').click(function () {
+        $('.loader').show(); 
+
+        var secondImageData = {
+            FirstName: $("#secondName1").val(),
+            LastName: $("#secondName2").val(),
+            Birthday: $("#secondDob").val(),
+            License: $("#secondLicense").val()
+        }
+ 
             $.ajax({
-                url: this.action,
-                type: this.method,
-                data: formdata,
+                url: "/Home/AnalyseImage2",
+                type: "post",
+                data: JSON.stringify(secondImageData),
                 processData: false,
-                contentType: false,
+                contentType: "application/json",
                 success: function (result) {
-                    $('#nameMatch').removeClass();
-                    $('#birthMatch').removeClass();
-                    $('#matchResult').removeClass();
-                    $('#nameMatch').addClass(result.NameMatch ? 'matchSuccess' : 'matchFail');
-                    $('#birthMatch').addClass(result.BirthdayMatch ? 'matchSuccess' : 'matchFail');
-                    $('#licenseMatch').addClass(result.LicenseMatch ? 'matchSuccess' : 'matchFail');
+                    $('#nameMatch2').removeClass();
+                    $('#birthMatch2').removeClass();
+                    $('#matchResult2').removeClass();
+                    $('#nameMatch2').addClass(result.NameMatch ? 'matchSuccess' : 'matchFail');
+                    $('#birthMatch2').addClass(result.BirthdayMatch ? 'matchSuccess' : 'matchFail');
+                    $('#licenseMatch2').addClass(result.LicenseMatch ? 'matchSuccess' : 'matchFail');
                     if (result.NameMatch && result.BirthdayMatch && result.LicenseMatch) {
-                        $('#matchResult').addClass('SuccessResult');
+                        $('#matchResult2').addClass('SuccessResult');
                     } else {
-                        $('#matchResult').addClass('FailResult');
+                        $('#matchResult2').addClass('FailResult');
                     }
-                    setTimeout(function() {
+                    setTimeout(function () {
                         $('.loader').hide();
-                        $('#resultPage').show();
+                        $('#resultPage2').show();
                     }, 1000);
                 },
                 complete: function () {
                 }
-            });
-        }
-        return false;
+            });  
     });
+
+    $('#secondBetterAnalyse').click(function () {
+        $('.loader').show();
+
+        var secondImageData = {
+            FirstName: $("#secondName1").val(),
+            LastName: $("#secondName2").val(),
+            Birthday: $("#secondDob").val(),
+            License: $("#secondLicense").val()
+        }
+
+        $.ajax({
+            url: "/Home/BetterAnalyseImage2",
+            type: "post",
+            data: JSON.stringify(secondImageData),
+            processData: false,
+            contentType: "application/json",
+            success: function (result) {
+                $('#nameMatch2').removeClass();
+                $('#birthMatch2').removeClass();
+                $('#matchResult2').removeClass();
+                $('#nameMatch2').addClass(result.NameMatch ? 'matchSuccess' : 'matchFail');
+                $('#birthMatch2').addClass(result.BirthdayMatch ? 'matchSuccess' : 'matchFail');
+                $('#licenseMatch2').addClass(result.LicenseMatch ? 'matchSuccess' : 'matchFail');
+                if (result.NameMatch && result.BirthdayMatch && result.LicenseMatch) {
+                    $('#matchResult2').addClass('SuccessResult');
+                } else {
+                    $('#matchResult2').addClass('FailResult');
+                }
+                setTimeout(function () {
+                    $('.loader').hide();
+                    $('#resultPage2').show();
+                }, 1000);
+            },
+            complete: function () {
+            }
+        });
+    });
+
+    
 
 });
